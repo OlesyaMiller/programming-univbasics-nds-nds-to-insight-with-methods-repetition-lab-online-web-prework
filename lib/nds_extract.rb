5fffd4fb73b55cb8c0bd2e201f3b2e6704c7fb7d
OLESYA MILLER <olesyam.miller@gmail.com>
2019-11-27 01:21:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,7 +49,7 @@ def total_gross(source)
   total = 0 
   index = 0 
   while index < source.length do 
-    total += source[directors_totals()[list_of_directors()]]
+    total += directors_totals[list_of_directors(index)]
   end
   index += 1 
 end
